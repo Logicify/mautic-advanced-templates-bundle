76edae2e1e0cdc0078f58508d77412eef2b4ae81
Mark Mulder <mark@markmulder.net>
2021-08-26 15:18:29 +0200

    compatible with Twig v3

--- Helper/TemplateProcessor.php
+++ Helper/TemplateProcessor.php
@@ -5,6 +5,10 @@ namespace MauticPlugin\MauticAdvancedTemplatesBundle\Helper;
 use MauticPlugin\MauticAdvancedTemplatesBundle\Feed\FeedFactory;
 use MauticPlugin\MauticCrmBundle\Integration\Salesforce\Object\Lead;
 use Psr\Log\LoggerInterface;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+use Twig\Loader\ChainLoader;
+use Twig\TwigFilter;
 
 class TemplateProcessor
 {
@@ -15,7 +19,7 @@ class TemplateProcessor
     protected $logger;
 
     /**
-     * @var \Twig_Environment
+     * @var Environment
      */
     private $twigEnv;
     private $twigDynamicContentLoader;
@@ -28,7 +32,7 @@ class TemplateProcessor
     /**
      * @var FeedFactory
      */
-    private $feedFactory;
+    private $feedFactory;   
 
     /**
      * TemplateProcessor constructor.
@@ -42,8 +46,8 @@ class TemplateProcessor
         $this->logger = $logger;
         $this->twigDynamicContentLoader = $twigDynamicContentLoader;
         $logger->debug('TemplateProcessor: created $twigDynamicContentLoader');
-        $this->twigEnv = new \Twig_Environment(new \Twig_Loader_Chain([
-            $twigDynamicContentLoader, new \Twig_Loader_Array([])
+        $this->twigEnv = new Environment(new ChainLoader([
+            $twigDynamicContentLoader, new ArrayLoader([])
         ]));
         $this->configureTwig($this->twigEnv);
         $this->feedFactory = $feedFactory;
@@ -67,16 +71,16 @@ class TemplateProcessor
         return $content;
     }
 
-    protected function configureTwig(\Twig_Environment $twig)
+    protected function configureTwig(Environment $twig)
     {
         // You might want to register some custom TWIG tags or functions here
 
         // TWIG filter json_decode
-        $twig->addFilter(new \Twig_SimpleFilter('json_decode', function ($string) {
+        $twig->addFilter(new TwigFilter('json_decode', function ($string) {
             return json_decode($string, true);
         }));
 
-        $twig->addFilter(new \Twig_SimpleFilter('rss', function () {
+        $twig->addFilter(new TwigFilter('rss', function () {
             return $this->feedFactory->getItems($this->lead['id'], func_get_args());
         }));
     }
